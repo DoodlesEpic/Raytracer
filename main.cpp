2583b2dfc658e029866c15eb5582417ea3b32942
DoodlesEpic <eduardomoraes84@yahoo.com.br>
2021-01-29 00:57:27 -0300

    Implementar primeira versão do programa utilizando raytracing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include "cor.h"
+#include "raio.h"
 #include "vector3.h"
 
 #include <iostream>
 
 int main(int, char **) {
-    const int larguraImagem = 1000;
-    const int alturaImagem = 1000;
+    // Configuração da tela da imagem
+    // Utilizar uma proporção de tela padrão, e inferir a altura a partir da largura
+    const double proporcaoTela = 16.0 / 9.0;
+    const int larguraImagem = 1280;
+    const int alturaImagem = static_cast<int>(larguraImagem / proporcaoTela);
+
+    // Configuração da câmera
+    // Inferir largura da janela a partir da proporção e altura
+    double alturaJanela = 2.0;
+    double larguraJanela = proporcaoTela * alturaJanela;
+    double distanciaFocal = 1.0;
+
+    // Ponto 0, onde estará localizada a câmera
+    ponto3 origem = ponto3(0, 0, 0);
+
+    // Vetores representando os eixo da tela, e o canto inferior esquerdo
+    vector3 horizontal = vector3(larguraJanela, 0, 0);
+    vector3 vertical = vector3(0, alturaJanela, 0);
+    vector3 cantoInferiorEsquerdo = origem - horizontal / 2 - vertical / 2 - vector3(0, 0, distanciaFocal);
 
     // Header da image PPM
     std::cout << "P3" << std::endl
@@ -19,8 +37,13 @@ int main(int, char **) {
 
         // Para cada pixel na scanline
         for (int i = 0; i < larguraImagem; ++i) {
-            // Calcular valores RGB para o pixel
-            cor corPixel(double(i) / (larguraImagem - 1), double(j) / (alturaImagem - 1), 0.25);
+            // Offsets em espaço UV para posicionar o raio
+            double u = double(i) / (larguraImagem - 1);
+            double v = double(j) / (alturaImagem - 1);
+
+            // Construir um raio e utilizá-lo para calcular a cor do pixel
+            raio raio(origem, cantoInferiorEsquerdo + u * horizontal + v * vertical - origem);
+            cor corPixel = corRaio(raio);
 
             // Printar resultado do pixel
             escreverCor(std::cout, corPixel);
