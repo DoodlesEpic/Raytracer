4ce8e3bec200932902ecf1005ade41269d83e39a
DoodlesEpic <eduardomoraes84@yahoo.com.br>
2021-02-06 20:31:02 -0300

    Implementar gerador de números aleatórios

--- util.h
+++ util.h
@@ -2,6 +2,7 @@
 #define RAYTRACER_UTIL_H
 
 #include <limits>
+#include <cstdlib>
 
 // Constantes globais
 const double infinito = std::numeric_limits<double>::infinity();
@@ -10,6 +11,16 @@ const double pi = 3.1415926535897932385;
 // Helpers globais
 inline double grausParaRadianos(double graus) {
     return graus * pi / 180.0;
+}
+
+// Retorna um número real em que 0 <= r < 1
+inline double doubleAleatorio() {
+    return rand() / (RAND_MAX + 1.0);
+}
+
+// Retorna um número real em que min <= r < max
+inline double doubleAleatorio(double min, double max) {
+    return min + (max - min) * doubleAleatorio();
 }
 
 #endif //RAYTRACER_UTIL_H
