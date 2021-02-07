cf9d758c6eb3a811e362410efaa3b8443262000b
DoodlesEpic <eduardomoraes84@yahoo.com.br>
2021-02-06 21:38:24 -0300

    Realizar correção de gamma na imagem

--- cor.h
+++ cor.h
@@ -9,15 +9,16 @@
 void escreverCor(std::ostream &out, cor corPixel, const int samplesPorPixel) {
     const double escala = 1.0 / samplesPorPixel;
 
-    // Pegar a cor de cada pixel, multiplicando pela escala
-    const double r = corPixel.x() * escala;
-    const double g = corPixel.y() * escala;
-    const double b = corPixel.z() * escala;
+    // Pegar a cor de cada pixel, dividindo pela escala
+    // Realizar correção de gamma para gamma 2
+    const double r = sqrt(corPixel.x() * escala);
+    const double g = sqrt(corPixel.y() * escala);
+    const double b = sqrt(corPixel.z() * escala);
 
     // Escrever as cores mapeadas entre 0 <= cor < 1
-    out << static_cast<int>(255.999 * clamp(r, 0.0, 0.999)) << ' '
-        << static_cast<int>(255.999 * clamp(g, 0.0, 0.999)) << ' '
-        << static_cast<int>(255.999 * clamp(b, 0.0, 0.999)) << '\n';
+    out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
 }
 
 #endif //RAYTRACER_COR_H
